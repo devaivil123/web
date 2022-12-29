32852295f661d7f4c446d4c99e7accd85e568307
devaivil123 <vanysha.vania@gmail.com>
2022-12-29 21:55:37 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -4,3 +4,13 @@ function showDate(params) {
     out.innerHTML = today.toLocaleDateString("ru-RU");
 }
 window.addEventListener('load', showDate);
+
+function showDaysCount (params){
+    let today = new Date ();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date (inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    daysCount = Math.floor(daysCount);
+    result.innerHTML = 'С даты рождения прошло дней:'+ daysCount; 
+}
