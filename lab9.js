3f393329a744d46436a217402a19d891bf91b741
devaivil123 <vanysha.vania@gmail.com>
2022-12-29 22:28:41 +0700

    Вывод координат мыши

--- lab9.js
+++ lab9.js
@@ -18,3 +18,37 @@ function Click3(button){
     else
         button.innerHTML = 'Имя Евгений'    
 }
+
+
+function Click4(button){
+    if(button.innerHTML == 'Нажми меня')
+        button.innerHTML = 'Ещё раз нажми';
+
+    else if(button.innerHTML == 'Ещё раз нажми')
+        button.innerHTML = 'Ещё';
+
+    else if(button.innerHTML == 'Ещё')
+        button.innerHTML = 'Начать сначала';
+    else
+        button.innerHTML = 'Нажми меня'
+}
+
+function showCoords(event) {
+    event.target.innerHTML = 'x = ' + event.offsetX + ', y = ' + event.offsetY;
+    if(event.ctrlKey) {
+        alert('Нажат Ctrl');
+    }
+
+    if (event.altKey) {
+		alert('Нажат Alt');
+    }
+
+	if (event.shiftKey) {
+		alert('Нажат Shift');
+    }
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    let coords = document.querySelector('#coords');
+    coords.addEventListener('mousemove', showCoords);
+});
