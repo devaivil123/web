b32872ecf280692e5c294fef8e322fd8af92895d
devaivil123 <vanysha.vania@gmail.com>
2022-12-29 22:39:22 +0700

    Изменение

--- lab9.js
+++ lab9.js
@@ -1,54 +1,63 @@
-function Click1(button) {
+function click1(button) {
     if(button.innerHTML == 'Щелчок')
-        button.innerHTML = 'Спасибо';
-    else
-        button.innerHTML = 'Щелчок'
+    button.innerHTML = 'Спасибо';
+    else 
+    button.innerHTML = 'Щелчок';
 }
 
-function Click2(button) {
-    if(button.innerHTML == 'Имя Иван')
-        button.innerHTML = 'Фамилия Шевчук';
+function click2(button) {
+    if(button.innerHTML == 'Иван')
+    button.innerHTML = 'Шевчук';
     else
-        button.innerHTML = 'Имя Иван'
+    button.innerHTML = 'Иван';
 }
 
-function Click3(button){
-    if(button.innerHTML == 'Имя Евгений')
-        button.innerHTML = 'Фамилия Демидов';
+function click3(button) {
+    if(button.innerHTML == 'Евгений')
+    button.innerHTML = 'Демидов';
     else
-        button.innerHTML = 'Имя Евгений'    
+    button.innerHTML = 'Евгений';
 }
 
-
-function Click4(button){
+function click4(button) {
     if(button.innerHTML == 'Нажми меня')
-        button.innerHTML = 'Ещё раз нажми';
+    button.innerHTML = 'Ещё раз нажми';
 
     else if(button.innerHTML == 'Ещё раз нажми')
-        button.innerHTML = 'Ещё';
+    button.innerHTML = 'Ещё';
 
     else if(button.innerHTML == 'Ещё')
-        button.innerHTML = 'Начать сначала';
+    button.innerHTML = 'Начать сначала';
     else
-        button.innerHTML = 'Нажми меня'
+    button.innerHTML = 'Нажми меня';
 }
 
 function showCoords(event) {
     event.target.innerHTML = 'x = ' + event.offsetX + ', y = ' + event.offsetY;
     if(event.ctrlKey) {
-        alert('Нажат Ctrl');
+        alert('Нажата клавиша Ctrl');
     }
 
-    if (event.altKey) {
-		alert('Нажат Alt');
+    if(event.altKey) {
+        alert('Нажата клавиша Alt');
     }
 
-	if (event.shiftKey) {
-		alert('Нажат Shift');
+    if(event.shiftKey) {
+        alert('Нажата клавиша Shift');
     }
 }
+
 
 window.addEventListener('DOMContentLoaded', function() {
     let coords = document.querySelector('#coords');
     coords.addEventListener('mousemove', showCoords);
 });
+
+function fokyc(tag) {
+    tag.style.color = "blue"
+
+    if(tag.style.color = "blue")
+        span.style.fontsize = "28px";
+    else
+        ag.style.color = "blue"
+}
